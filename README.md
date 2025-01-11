2b1daafa603e0b96db1905ee27c77c3a55fa7708
Kamil Jan Mularski <kammul@outlook.com>
2025-01-11 23:11:49 +0100

    Create Request.java

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Simple Request Service
 Projekt dostarcza interfejs dostawcy serwisu wysyłania żądań HTTP.
 
+## Tworzenie żądań
+Przykładowy kod tworzący nowe żądanie:
+
+```java
+Request request = Request.builder()
+        .method(Method.GET) // domyślna wartość
+        .path("/index.html") // wartość domyślna: "/"
+        .protocol(Protocol.HTTP_1_1) // domyślna wartość
+        .header("Content-type", "text/html")
+        .body("") // domyślna wartość
+        .build();
+```
---
+++ src/main/java/pl/koder95/srs/Request.java
@@ -0,0 +1,77 @@
+package pl.koder95.srs;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Data
+public final class Request {
+
+    private final Method method;
+    private final String path;
+    private final Protocol protocol;
+    private final Set<Header> headers;
+    private final String body;
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Method method;
+        private String path;
+        private Protocol protocol;
+        private final Set<Header> headers = new LinkedHashSet<>();
+        private String body;
+
+        private Builder() {}
+
+        public Builder method(Method method) {
+            this.method = method;
+            return this;
+        }
+
+        public Builder path(String path) {
+            this.path = path;
+            return this;
+        }
+
+        public Builder protocol(Protocol protocol) {
+            this.protocol = protocol;
+            return this;
+        }
+
+        public Builder header(String key, String value) {
+            this.headers.add(new Header(key, value));
+            return this;
+        }
+
+        public Builder body(String body) {
+            this.body = body;
+            return this;
+        }
+
+        public Request build() {
+            return new Request(
+                    this.method == null? Method.GET : this.method,
+                    this.path == null || this.path.isEmpty()? "/" : this.path,
+                    this.protocol == null? Protocol.HTTP_1_1 : this.protocol,
+                    Collections.unmodifiableSet(this.headers),
+                    this.body == null? "" : this.body);
+        }
+
+        public String toString() {
+            return "Request.Builder(" +
+                    "method=" + this.method + ", " +
+                    "path=" + this.path + ", " +
+                    "protocol=" + this.protocol + ", " +
+                    "headers=" + this.headers + ", " +
+                    "body=" + this.body + ")";
+        }
+    }
+}
