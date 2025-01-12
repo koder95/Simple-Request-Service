4dc5c5eeaf746441897204f03277dfd597acc8f4
Kamil Jan Mularski <kammul@outlook.com>
2025-01-12 01:15:45 +0100

    Create Service.java

---
+++ src/main/java/pl/koder95/srs/Service.java
@@ -0,0 +1,43 @@
+package pl.koder95.srs;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Queue;
+
+@RequiredArgsConstructor
+public class Service implements Runnable {
+
+    @Getter
+    private final String name;
+    @Getter
+    private final CallHandler callHandler;
+    @Getter
+    private final Callback callback;
+
+    private final Queue<Request> requestQueue;
+    private boolean closeRequest = false;
+
+    public final Thread start() {
+        Thread thread = new Thread(this, getName() + " #" + Long.toHexString(System.currentTimeMillis()));
+        thread.setDaemon(true);
+        thread.start();
+        return thread;
+    }
+
+    @Override
+    public void run() {
+        while (!closeRequest) {
+            if (!requestQueue.isEmpty())
+                callHandler.accept(requestQueue.poll(), callback);
+        }
+    }
+
+    public void close() {
+        this.closeRequest = true;
+    }
+
+    public boolean enqueue(Request request) {
+        return requestQueue.add(request);
+    }
+}
